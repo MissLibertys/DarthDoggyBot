80a9d09e72c38223b8df958b6206e4c18a365c15
DarthDoggys <46267430+DarthDoggys@users.noreply.github.com>
2018-12-30 11:04:31 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,38 @@
+import discord
+from discord.ext import commands
+import asyncio
+import time
+from itertools import cycle
+import os
 
+client = commands.Bot(command_prefix=("m."))
+status = ["testing the bot", "m.help"]
+
+async def change_status():
+  await client.wait_until_ready()
+  msgs = cycle(status)
+  
+  while not client.is_closed:
+    current_status = next(msgs)
+    await client.change_presence(game=discord.Game(name=current_status))
+    await asyncio.sleep(5)
+    
+player = {}	
+
+@client.event
+async def on_ready():
+	print('Logged in as')
+	print("User name:", client.user.name)
+	print("User id:", client.user.id)
+	print('---------------')
+    
+@client.command(pass_context=True)
+async def ping(ctx):
+    """Pings the bot and gets a response time."""
+    pingtime = time.time()
+    pingms = await client.say("Pinging...")
+    ping = (time.time() - pingtime) * 1000
+    await client.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
+	
+client.loop.create_task(change_status())
+client.run(os.environ['BOT_TOKEN'])
