92bd864368e2ff8405c62929e7cb1feac74e4a9f
DarthDoggys <46267430+DarthDoggys@users.noreply.github.com>
2018-12-30 11:15:56 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -33,6 +33,85 @@ async def ping(ctx):
     pingms = await client.say("Pinging...")
     ping = (time.time() - pingtime) * 1000
     await client.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
+
+@client.command(pass_context=True)
+@commands.has_permissions(kick_members=True, administrator=True)
+async def mute(ctx, user: discord.Member, *, arg):
+	if arg is None:
+		await client.say("please say a reason to {}".format(user.name))
+		return False
+	reason = arg
+	author = ctx.message.author
+	role = discord.utils.get(ctx.message.server.roles, name="Muted")
+	await client.add_roles(user, role)
+	embed = discord.Embed(title="Mute", description=" ", color=0xFFA500)
+	embed.add_field(name="User: ", value="<@{}>".format(user.id), inline=False)
+	embed.add_field(name="Moderator: ", value="{}".format(author.mention), inline=False)
+	embed.add_field(name="Reason: ", value="{}\n".format(arg), inline=False)
+	await client.say(embed=embed)
+	
+@client.command(pass_context=True)
+@commands.has_permissions(kick_members=True, administrator=True)
+async def unmute(ctx, user: discord.Member, *, arg):
+	if arg is None:
+		await client.say("please say a reason to {}".format(user.name))
+		return False
+	reason = arg
+	author = ctx.message.author
+	role = discord.utils.get(ctx.message.server.roles, name="Muted")
+	await client.remove_roles(user, role)
+	embed = discord.Embed(title="Unmute", description=" ", color=0x00ff00)
+	embed.add_field(name="User: ", value="<@{}>".format(user.id), inline=False)
+	embed.add_field(name="Moderator: ", value="{}".format(author.mention), inline=False)
+	embed.add_field(name="Reason: ", value="{}\n".format(arg), inline=False)
+	await client.say(embed=embed)
+
+@client.command(pass_context=True)
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, user: discord.Member, *, arg):
+	if arg is None:
+		await client.say("please say a reason to {}".format(user.name))
+		return False
+	reason = arg
+	author = ctx.message.author
+	await client.kick(user)
+	embed = discord.Embed(title="Kick", description=" ", color=0x00ff00)
+	embed.add_field(name="User: ", value="<@{}>".format(user.id), inline=False)
+	embed.add_field(name="Moderator: ", value="{}".format(author.mention), inline=False)
+	embed.add_field(name="Reason: ", value="{}\n".format(arg), inline=False)
+	await client.say(embed=embed)
+  
+@client.command(pass_context=True)
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, user: discord.Member, *, arg):
+	if arg is None:
+		await client.say("please say a reason to {}".format(user.name))
+		return False
+	reason = arg
+	author = ctx.message.author
+	await client.ban(user)
+	embed = discord.Embed(title="Ban", description=" ", color=0xFF0000)
+	embed.add_field(name="User: ", value="<@{}>".format(user.id), inline=False)
+	embed.add_field(name="Moderator: ", value="{}".format(author.mention), inline=False)
+	embed.add_field(name="Reason: ", value="{}\n".format(arg), inline=False)
+	await client.say(embed=embed)
+	
+@client.command(pass_context=True)
+@commands.has_permissions(kick_members=True)
+async def warn(ctx, user: discord.Member, *, arg = None):
+	if arg is None:
+		await clieng.say("please say a reason to {}".format(user.name))
+		return False
+	reason = arg
+	author = ctx.message.author
+	server = ctx.message.server
+	embed = discord.Embed(title="Warn", description=" ", color=0x00ff00)
+	embed.add_field(name="User: ", value="<@{}>".format(user.id), inline=False)
+	embed.add_field(name="Moderator: ", value="{}".format(author.mention), inline=False)
+	embed.add_field(name="Reason: ", value="{}\n".format(arg), inline=False)
+	await client.say(embed=embed)
+	await client.send_message(user, "You have been warned for: {}".format(reason))
+	await client.send_message(user, "from: {} server".format(server))
 	
 client.loop.create_task(change_status())
 client.run(os.environ['BOT_TOKEN'])
